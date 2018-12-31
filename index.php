38fea3cb94886c740d33f474c75becc77192f338
ayushmyan <thakuriay@mnstate.edu>
2018-12-31 12:48:16 -0600

    php array

--- index.php
+++ index.php
@@ -1,109 +1,67 @@
-<form>
+
 <?php
-extract($_REQUEST);
-    
-function namepage()
-{
-	echo <<< HERE
-  <h1> Division Quiz </h1>
-  <h2> Enter your name: <input type="text" name= "name" autocomplete="off"> </h2>
-  	
-  <br>
-  <input type="submit" name="button" value="Start">  <br> <br>
-  <a href="../index.html"> Main Page </a>
-HERE;
+/*
+ * The goal is to use a combination of multiple loops, functions, and nested arrays (multidimensional arrays)
+ * to output a form to let a user pick date information.
+ *
+ * In addition to doing this as it's own assignment
+ * You'll want to modify this in order to use it in the next assignment.
+ */
 
-}
+function selectArray($name,$theArray) {
+	// Based slightly off of https://github.com/andrew-chen/csis311/blob/master/examples/selectArray.php
 
-function querypage($name,$rand,$rand1,$right,$num)
-{
+	echo "<select name=\"$name\">";
+	/* loop through $theArray and have the following loop body work on every $key => $value) */ 
+	foreach ($theArray as $key => $value){
+	
+		echo "<option value=\"$key\">$value</option>";
+	};
+	echo "</select>";
+	
+	
+	
+};
 
- echo <<< HERE
- <h1> Participant: $name </h1>
- <h2> $rand / $rand1 = <input type="text" name="answer" autocomplete="off"> </h2>
- <br>
- <input type="submit" name="button" value="Submit">
- <input type="submit" name="button" value="Restart">
+function selectNum($name,$first,$last) {
+// Based slightly off of https://github.com/andrew-chen/csis311/blob/master/examples/selectNum.php
+	echo "<select name=\"$name\">";
+/* have $i go from $first to $last */
+	for ($i=$first; $i<=$last; $i++){
+	
+		echo "<option>$i</option>";
+	};
+	echo "</select>";
+};
 
- <br><br>
- Number of correct answers: $right <br>
- Number of problems attempted: $num <br>
-
-
-HERE;
-echo "<input type='hidden' name='name' value='$name'>";
-echo "<input type='hidden' name='rand' value='$rand'>";
-echo "<input type='hidden' name='rand1' value='$rand1'>";
-echo "<input type='hidden' name='right' value='$right'>";
-echo "<input type='hidden' name='num' value='$num'>";
-}
-
-function check($name,$answer,$rand,$rand1,&$right,&$num)
-{
-	if ($rand / $rand1 == $answer){
-		$right++;
-		$num++;
+function form($layout) {
+	echo "<form>";
+/* loop through $layout and have the following loop body work on every $key => $value) */ 
+	foreach ($layout as $key =>$value){
 		
-		echo <<< HERE
-		<h3> CORRECT! </h3>
-		$rand / $rand1 = $answer <br><br>
-		<input type="submit" name="button" value="Next Problem"> <br><br>
-      Number of correct answers: $right <br>
-      Number of problems attempted: $num <br>
-HERE;
+		if (is_array($value)) {
+		if ((count($value) == 2)&&(is_numeric($value[0]))&&(is_numeric($value[1]))) {
+/* call selectNum with $key as the $name and with $value[0] as $first and with $value[1] as $last */
+				selectNum($key,$value[0],$value[1]);
+			} else {
+/* call selectArray with $key as the $name and with $value as $theArray */
+				selectArray($key,$value);
+			};
+		} else {
+/* call selectNum with $key as the $name and with 1 as $first and with $value as $last */
+			selectNum($key,1,$value);
+		};
+	};
+	echo "<input type=\"submit\"></form>";
+};
 
-	}
-	
-	else{
-		$num++;
-		echo <<< HERE
-      <h1> INCORRECT!</h1>
-      <input type="submit" name="button" value="Try Again"><br><br>
-	  <input type="submit" name="button" value="Restart"><br><br>
-      Number of correct answers: $right <br>
-      Number of problems attempted: $num <br>
-      
-HERE;
-	}
-	
-	echo "<input type='hidden' name='name' value='$name'>";
-	echo "<input type='hidden' name='right' value='$right'>";
-	echo "<input type='hidden' name='num' value='$num'>";
-	echo "<input type='hidden' name='rand' value='$rand'>";
-	echo "<input type='hidden' name='rand1' value='$rand1'>";
-}
-
-
-   
-if ($button==NULL or $button =="Restart") {
-namepage();}
-
-else if ($button == "Start"){
-		$rand = rand(1,9);
-		$rand1 = rand(1,9);
-		$right =0;
-		$num =0;
-    querypage($name,$rand,$rand1,$right,$num);}
-
-else if ($button == "Next Problem"){
-	$rand = rand(1,9);
-	$rand1 = rand(1,9);
-	querypage($name,$rand,$rand1,$right,$num);}
-	
-else if ($button == "Try Again"){
-	querypage ($name,$rand,$rand1,$right,$num);}
-	
-else if ($button == "Submit"){
-	check($name,$answer,$rand,$rand1,$right,$num);   
-}
-
+if (count($_REQUEST)) {
+	print_r($_REQUEST);
+} else {
+	form(array("year"=>array(2010,2020),"month"=>array("January","February","March","April","May","June","July","August","September","October","November","December"),"day"=>31,"hour"=>24,"minute"=>60));
+};
 ?>
-</form>
 
- 
-
-
-<?php
-echo "<HR>";
-highlight_file("index.php"); ?>
-
+<p><a href="../.">Back to main page.</a></p>
+</body>
+</html>
